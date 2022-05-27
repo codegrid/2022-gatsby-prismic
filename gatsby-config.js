e44bb1fdfece38f3b18b6d426524a115838bb2b4
Takeshi Takatsudo <takazudo@gmail.com>
2022-05-27 20:32:40 +0900

    Prismicのデータを読み込む設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `codegrid-prismic-demo`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        schemas: {
+          news: require("./custom_types/news.json"),
+        },
+      },
+    },
+  ],
+};
