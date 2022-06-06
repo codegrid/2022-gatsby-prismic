440c8f8655f573ae6872191688249ae083323301
Takeshi Takatsudo <takazudo@gmail.com>
2022-06-06 16:14:20 +0900

    データを画面表示に反映

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,10 +19,18 @@ export const query = graphql`
 `;
 
 const IndexPage = ({ data }) => {
-  console.log(data);
+  // console.log(data);
   return (
     <main>
       <h1>News List</h1>
+      {data.allPrismicNews.edges.map((item) => {
+        const { uid, data } = item.node;
+        return (
+          <li key={uid}>
+            <a href={`/news/${uid}`}>{data.title.text}</a>
+          </li>
+        );
+      })}
     </main>
   )
 }
